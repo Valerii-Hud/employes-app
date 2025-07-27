cd39e117a8504a4d7d6fd6aee9cf723e9a6793f4
Valerii Hud <support@valerii-hud.com>
2025-07-27 21:14:39 +0200

    added like btn

--- src/components/employeers-list-item/employeers-list-item.js
+++ src/components/employeers-list-item/employeers-list-item.js
@@ -6,6 +6,7 @@ class EmployeersListItem extends Component {
     super(props);
     this.state = {
       increase: false,
+      like: false,
     };
   }
   toggleIncrease = () => {
@@ -13,16 +14,25 @@ class EmployeersListItem extends Component {
       increase: !increase,
     }));
   };
+  toggleLike = (e) => {
+    this.setState(({ like }) => ({
+      like: !like,
+    }));
+  };
   render() {
     const { name, surname, salary } = this.props;
-    const { increase } = this.state;
+    const { increase, like } = this.state;
+
     let classNames = 'list-group-item d-flex justify-content-between ';
     if (increase) {
       classNames += ' increase';
     }
+    if (like) {
+      classNames += ' like';
+    }
     return (
       <li className={classNames}>
-        <span className="list-group-item-label">
+        <span className="list-group-item-label" onClick={this.toggleLike}>
           {name} {surname}
         </span>
         <input
